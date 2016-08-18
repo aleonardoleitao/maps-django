60b44d5a1850547c89145189a8dd4b6c690c646e
Andre <aleonardo.leitao@gmail.com>
2016-08-18 04:17:33 -0300

    novo path

--- localidades/urls.py
+++ localidades/urls.py
@@ -17,7 +17,7 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from maps.app import views
+from localidades.app import views
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
